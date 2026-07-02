830cdf67670f7d95781cfe2d02024a7ed6fbbce5
Judenause <Judenause@users.noreply.github.com>
2026-07-02 09:49:00 +0900

    Use Lavie Foret location image

--- script.js
+++ script.js
@@ -472,7 +472,7 @@
     $('#locationHall').textContent = w.hall;
     $('#locationAddress').textContent = w.address;
     $('#locationTel').textContent = w.tel ? `Tel. ${w.tel}` : '';
-    $('#locationMapImg').src = 'images/location/1.jpg';
+    $('#locationMapImg').src = 'images/location/라뷔포레.jpg';
     $('#kakaoMapBtn').href = w.mapLinks.kakao || '#';
     $('#naverMapBtn').href = w.mapLinks.naver || '#';
 
